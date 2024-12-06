2252506208f1052adf663a5509e05fb2ed9b2b8f
una0801 <una801@naver.com>
2024-12-06 15:27:34 +0900

    Title: 3N + 1 수열, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241206/3N + 1 수열/3n-plus-1-sequence.py
+++ 241206/3N + 1 수열/3n-plus-1-sequence.py
@@ -1,11 +1,14 @@
 a = int(input())
 cnt = 0
 while True:
-    if a%2==0:
-        a//=2
-    else : a = a*3+1
-    cnt +=1
     if a ==1:
         break
+    if a%2==0:
+        a//=2
+        cnt+=1
+    else: 
+        a = a*3+1
+        cnt +=1
+
 
 print(cnt)
