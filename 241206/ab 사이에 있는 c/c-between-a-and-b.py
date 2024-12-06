83b4c70af029db1128f239e5aa10a7889ba48d83
una0801 <una801@naver.com>
2024-12-06 15:52:46 +0900

    Title: ab 사이에 있는 c, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241206/ab 사이에 있는 c/c-between-a-and-b.py
@@ -0,0 +1,16 @@
+
+inp = input()
+arr = inp.split()
+a, b, c = int(arr[0]), int(arr[1]), int(arr[2])
+satisfied = False
+
+for i in range(a, b + 1):
+	if i % c == 0:
+		satisfied = True
+
+
+
+if satisfied == True:
+	print("YES")
+else:
+	print("NO")
