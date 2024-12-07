99c535b92b843b911049042450a3f07e13ba7ef2
una0801 <una801@naver.com>
2024-12-07 13:59:24 +0900

    Title: ab 사이에 없는 c, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241207/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,10 @@
+a,b,c= map(int,input().split())
+satisfied = False
+for i in range(a,b+1):
+    if i%c ==0:
+        satisfied = True
+        break
+
+if satisfied:
+    print('NO')
+else:print('YES')
